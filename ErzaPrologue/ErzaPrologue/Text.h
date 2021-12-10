c353b556e04ef5b518cdd3763d5cea36f299a08f
Nazarii <nazariyproshyn@gmail.com>
2021-12-10 03:02:18 +0200

    Add Text Class

---
+++ ErzaPrologue/ErzaPrologue/Text.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+class Text
+{
+	Text()  = default;
+	~Text() = default;
+	void setSize(int size) { text.setCharacterSize(size); }
+	void setBorderColour(const sf::Color& colour) { text.setOutlineColor(colour); };
+	void setTextColour(const sf::Color& colour) { text.setFillColor(colour); };
+	void setBorderThickness(float size) { text.setOutlineThickness(size); };
+	void setFont(const sf::Font& font) { text.setFont(font); }
+	void setPosition(float x, float y) { text.setPosition(x, y); }
+private:
+	sf::Text text;
+};
+
