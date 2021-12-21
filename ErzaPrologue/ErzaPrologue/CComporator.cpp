125cabdd1f2b4abecf534d2125e8eaedb2b8318d
Volodymyr Hrytsiuk <vhrytsiuk@corelogic.com>
2021-12-21 19:18:04 +0200

    comporator

---
+++ ErzaPrologue/ErzaPrologue/CComporator.cpp
@@ -0,0 +1,7 @@
+#include "CComporator.h"
+
+template <class T>
+bool CComporator<T>::isEqual(const const T& t1, const T& t2)
+{
+	return t1 == t2;
+}
---
+++ ErzaPrologue/ErzaPrologue/CComporator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+template <class T>
+class CComporator
+{
+public:
+	static bool isEqual(const T& t1, const T& t);
+};
+
